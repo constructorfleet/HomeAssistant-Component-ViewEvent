ab7688c1aaff5d4e5588ce7c62f041f042f20c28
Teagan glenn <teagan.m.glenn@gmail.com>
2019-10-20 23:28:43 -0600

    Revert "Send name for view"
    This reverts commit 55c8b9a46082ce1dc41bf68fca304cdb74671e79.

--- __init__.py
+++ __init__.py
@@ -4,25 +4,25 @@ Fire an event when an API route is registered.
 For more details about this component, please refer to the documentation at
 https://home-assistant.io/components/remote_homeassistant/
 """
+import asyncio
+import gc
 import logging
-
-import homeassistant.helpers.config_validation as cv
 import voluptuous as vol
-from homeassistant.components import websocket_api
+
 from homeassistant.components.http import HomeAssistantView
+from homeassistant.components import websocket_api
 from homeassistant.core import callback
+import homeassistant.helpers.config_validation as cv
 
 _LOGGER = logging.getLogger(__name__)
 
 ATTR_METHOD = 'method'
 ATTR_AUTH_REQUIRED = 'auth_required'
-ATTR_REMOTE_INSTANCE = 'remote_instance'
 EVENT_TYPE_REQUEST_ROUTES = 'request_routes'
 EVENT_TYPE_ROUTE_REGISTERED = 'route_registered'
 ATTR_ROUTE = 'route'
 
 CONF_COMPONENTS = 'components'
-CONF_INSTANCE_NAME = 'name'
 
 DOMAIN = 'view_event'
 
@@ -33,30 +33,32 @@ SCHEMA_REQUEST_ROUTES = \
 
 CONFIG_SCHEMA = vol.Schema({
     DOMAIN: vol.Schema({
-        vol.Required(CONF_INSTANCE_NAME): cv.string,
         vol.Required(CONF_COMPONENTS): vol.All(cv.ensure_list,
                                                [cv.slugify])
     }),
 }, extra=vol.ALLOW_EXTRA)
 
 
-def _get_routes(instance_name, view, components):
+def _get_routes(view, components):
     urls = [view.url] + view.extra_urls
     routes = []
 
     for method in ["get", "post", "delete", "put", "patch", "head", "options"]:
+        _LOGGER.warn("Checking for handler for %s" % method)
         handler = getattr(view, method, None)
 
         if not handler:
             continue
+        _LOGGER.warning("Components %s" % str(components))
         for url in urls:
+            _LOGGER.warn("Checking if should register %s" % url)
+            _LOGGER.warning("URL in components?? %s" % str(any(component in url for component in components)))
             if not any(component in url for component in components):
                 continue
             routes.append({
                 ATTR_ROUTE: url,
                 ATTR_METHOD: method,
-                ATTR_AUTH_REQUIRED: view.requires_auth,
-                ATTR_REMOTE_INSTANCE: instance_name
+                ATTR_AUTH_REQUIRED: view.requires_auth
             })
 
     return routes
@@ -78,8 +80,8 @@ class ViewEvent(object):
 
     def __init__(self, hass, conf):
         self._hass = hass
+        _LOGGER.warning(str(conf))
         self._components = conf[DOMAIN][CONF_COMPONENTS]
-        self._name = conf[DOMAIN][CONF_INSTANCE_NAME]
         hass.components.websocket_api.async_register_command(
             EVENT_TYPE_REQUEST_ROUTES,
             self._routes_requested_handler,
@@ -88,6 +90,7 @@ class ViewEvent(object):
         HomeAssistantView.register = self._wrap_function(
             HomeAssistantView.register
         )
+        # asyncio.ensure_future(self._get_already_registered_routes())
 
     @callback
     def _routes_requested_handler(self, hass, connection, msg):
@@ -96,17 +99,22 @@ class ViewEvent(object):
             self._fire_event(route)
 
     def _handle_view_registration(self, view):
-        routes = _get_routes(self._name, view, self._components)
+        _LOGGER.warning("VIEW %s" % view.__class__.__name__)
+        routes = _get_routes(view, self._components)
+        _LOGGER.warning("ROUTES %s" % str(routes))
         for route in routes:
             self._handle_route_registration(route)
 
     def _handle_route_registration(self, route):
         if not self.send_routes:
+            _LOGGER.warning("ADDING TO LIST")
             self.registered_routes.append(route)
         else:
+            _LOGGER.warning("FIRING EVENT")
             self._fire_event(route)
 
     def _fire_event(self, route):
+        _LOGGER.warning("SENDING")
         self._hass.bus.async_fire(
             event_type=EVENT_TYPE_ROUTE_REGISTERED,
             event_data=route
@@ -117,7 +125,11 @@ class ViewEvent(object):
 
         def _w(view, app, router):
             """Execute wrapped function."""
+            _LOGGER.warning("Entering wrapper")
+            _LOGGER.warning("Invoking original")
             result = function(view, app, router)
+            _LOGGER.warning("GOT %s" % str(result))
+
             try:
                 self._handle_view_registration(view)
             except Exception as e:
@@ -132,6 +144,5 @@ class ViewEvent(object):
             self._handle_route_registration({
                 ATTR_ROUTE: route.canonical,
                 ATTR_METHOD: route.method,
-                ATTR_AUTH_REQUIRED: False,
-                ATTR_REMOTE_INSTANCE: self._name
+                ATTR_AUTH_REQUIRED: False
             })
