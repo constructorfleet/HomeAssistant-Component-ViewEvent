3b2b64b29c2923f9d40f85a8a3fc2f6aaa6199b3
Teagan glenn <teagan.m.glenn@gmail.com>
2019-10-20 23:38:43 -0600

    Pass instance name

--- __init__.py
+++ __init__.py
@@ -4,25 +4,25 @@ Fire an event when an API route is registered.
 For more details about this component, please refer to the documentation at
 https://home-assistant.io/components/remote_homeassistant/
 """
-import asyncio
-import gc
 import logging
-import voluptuous as vol
 
-from homeassistant.components.http import HomeAssistantView
-from homeassistant.components import websocket_api
-from homeassistant.core import callback
 import homeassistant.helpers.config_validation as cv
+import voluptuous as vol
+from homeassistant.components import websocket_api
+from homeassistant.components.http import HomeAssistantView
+from homeassistant.core import callback
 
 _LOGGER = logging.getLogger(__name__)
 
 ATTR_METHOD = 'method'
+ATTR_ROUTE = 'route'
 ATTR_AUTH_REQUIRED = 'auth_required'
+ATTR_INSTANCE_NAME = 'instance_name'
 EVENT_TYPE_REQUEST_ROUTES = 'request_routes'
 EVENT_TYPE_ROUTE_REGISTERED = 'route_registered'
-ATTR_ROUTE = 'route'
 
 CONF_COMPONENTS = 'components'
+CONF_NAME = 'name'
 
 DOMAIN = 'view_event'
 
@@ -39,7 +39,7 @@ CONFIG_SCHEMA = vol.Schema({
 }, extra=vol.ALLOW_EXTRA)
 
 
-def _get_routes(view, components):
+def _get_routes(instance_name, view, components):
     urls = [view.url] + view.extra_urls
     routes = []
 
@@ -58,7 +58,8 @@ def _get_routes(view, components):
             routes.append({
                 ATTR_ROUTE: url,
                 ATTR_METHOD: method,
-                ATTR_AUTH_REQUIRED: view.requires_auth
+                ATTR_AUTH_REQUIRED: view.requires_auth,
+                ATTR_INSTANCE_NAME: instance_name
             })
 
     return routes
@@ -80,8 +81,8 @@ class ViewEvent(object):
 
     def __init__(self, hass, conf):
         self._hass = hass
-        _LOGGER.warning(str(conf))
         self._components = conf[DOMAIN][CONF_COMPONENTS]
+        self._name = conf[DOMAIN][CONF_NAME]
         hass.components.websocket_api.async_register_command(
             EVENT_TYPE_REQUEST_ROUTES,
             self._routes_requested_handler,
@@ -99,18 +100,14 @@ class ViewEvent(object):
             self._fire_event(route)
 
     def _handle_view_registration(self, view):
-        _LOGGER.warning("VIEW %s" % view.__class__.__name__)
         routes = _get_routes(view, self._components)
-        _LOGGER.warning("ROUTES %s" % str(routes))
         for route in routes:
             self._handle_route_registration(route)
 
     def _handle_route_registration(self, route):
         if not self.send_routes:
-            _LOGGER.warning("ADDING TO LIST")
             self.registered_routes.append(route)
         else:
-            _LOGGER.warning("FIRING EVENT")
             self._fire_event(route)
 
     def _fire_event(self, route):
@@ -144,5 +141,6 @@ class ViewEvent(object):
             self._handle_route_registration({
                 ATTR_ROUTE: route.canonical,
                 ATTR_METHOD: route.method,
-                ATTR_AUTH_REQUIRED: False
+                ATTR_AUTH_REQUIRED: False,
+                ATTR_INSTANCE_NAME: self._name
             })
